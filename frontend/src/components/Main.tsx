5a215e473152b54b3be9c9828b146a73108b8f63
emoto <emotaka39@gmail.com>
2024-05-03 13:26:05 +0900

    フロントエンド_タスク登録編集削除の実装

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,10 +1,36 @@
 import Header from "./Header";
+import TaskTable from "./TaskTable";
+import { Payload } from "../types/payload";
+import { useQuery } from "@apollo/client";
+import { GET_TASKS } from "../queries/taskQueries";
+import { Task } from "../types/task";
+import Loading from "./Loading";
+import { Stack, Typography } from "@mui/material";
+import AddTask from "./AddTask";
+import { jwtDecode } from "jwt-decode";
 
 const Main = () => {
+  const token = localStorage.getItem("token");
+  const decodedToken = jwtDecode<Payload>(token!);
+  const userId = decodedToken.sub;
+
+  const { loading, data, error } = useQuery<{ getTasks: Task[] }>(GET_TASKS, {
+    variables: { userId },
+  });
+
   return (
     <>
       <Header />
-      <div>main</div>
+      <Stack spacing={4} direction="column" m={8} alignItems="center">
+        {loading && <Loading />}
+        {error && <Typography color="red">エラーが発生しました</Typography>}
+        {!loading && !error && (
+          <>
+            <AddTask userId={userId} />
+            <TaskTable tasks={data?.getTasks} userId={userId} />
+          </>
+        )}
+      </Stack>
     </>
   );
 };
